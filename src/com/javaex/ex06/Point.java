abd527c8a6b1c69aeb676b000fe4433aed8e3f4a
가은 <rkdms8228@naver.com>
2022-05-01 22:00:46 +0900

    ex06

--- src/com/javaex/ex06/Point.java
+++ src/com/javaex/ex06/Point.java
@@ -2,14 +2,17 @@ package com.javaex.ex06;
 
 public class Point {
 	
+	//필드
 	private int x;
 	private int y;
+	
+	//생성자
+	public Point() {
+	}
 	
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-
-	
 	
 }
--- src/com/javaex/ex06/PointApp.java
+++ src/com/javaex/ex06/PointApp.java
@@ -15,6 +15,12 @@ public class PointApp {
 		System.out.println(p4 == p1);		
 		System.out.println(p1.equals(p2));
 		System.out.println(p4.equals(p1));
+		
+		//object 클래스가 원래 존재하기 때문이다.
+		//object 클래스에는 equals 메소드가 있기 때문에 출력에 equals을 넣어도 오류가 없다.
+		//단, ==와 equals은 엄연히 다르기에 주의해야 한다.
+		//==은 주소값만을 비교하고
+		//하지만 equals는 메소드에서 값을 비교할 수 있도록 설정하여 비교를 하면 동일 값을 파악한다.
 	}
 
 }
