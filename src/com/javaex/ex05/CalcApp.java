97bfbe09fdbe6049e1714f2b98639eb5b05dd842
가은 <rkdms8228@naver.com>
2022-05-01 22:01:19 +0900

    수정 필요

--- src/com/javaex/ex05/CalcApp.java
+++ src/com/javaex/ex05/CalcApp.java
@@ -1,8 +1,50 @@
 package com.javaex.ex05;
 
+import java.util.Scanner;
+
 public class CalcApp {
 
     public static void main(String[] args) {
+    	
+    	Scanner sc = new Scanner(System.in);
+    	
+    	System.out.print(">> ");
+    	String cal = sc.nextLine();
+    	
+    	cal.trim();
+    	
+    	int a, b;
+    	
+    	String[] cArray = new String[3];
+    	cArray[0] = a;
+    	cArray[2] = b;
+    	
+    	switch(cArray[1]) {
+			case "+":
+				Add add = new Add();
+				System.out.println(">> "+add.calculate());
+				break;
+			case "-":
+				Sub sub = new Sub();
+				System.out.println(">> "+sub.calculate());
+				break;
+			case "*":
+				Mul mul = new Mul();
+				System.out.println(">> "+mul.calculate());
+				break;
+			case "/":
+				Div div = new Div();
+				System.out.println(">> "+div.calculate());
+				break;
+			case "/q":
+				System.out.println("종료합니다.");
+				break;
+			default:
+				System.out.println("알 수 없는 연산입니다.");
+				break;
+	}
+    	
+    	sc.close();
 
     }
 }
--- src/com/javaex/ex05/Div.java
+++ src/com/javaex/ex05/Div.java
@@ -2,13 +2,22 @@ package com.javaex.ex05;
 
 public class Div {
 
+	//필드
     private int a;
     private int b;
     
+    //생성자
+    public void setValue() {
+    }
+    
     public void setValue(int a, int b) {
+    	this.a = a;
+    	this.b = b;
     }
 
+    //메소드-일반
     public int calculate() {
+    	return a/b;
     }
 
 }
