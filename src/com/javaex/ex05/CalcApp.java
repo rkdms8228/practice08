3d08231957fbd81be33be595eabc22de621ea94d
가은 <rkdms8228@naver.com>
2022-05-03 00:02:49 +0900

    2차 수정중

--- src/com/javaex/ex05/CalcApp.java
+++ src/com/javaex/ex05/CalcApp.java
@@ -16,24 +16,29 @@ public class CalcApp {
     	int a, b;
     	
     	String[] cArray = new String[3];
-    	cArray[0] = a;
-    	cArray[2] = b;
+    	a = Integer.parseInt(cArray[0]);
+    	b = Integer.parseInt(cArray[2]);
+
     	
     	switch(cArray[1]) {
 			case "+":
 				Add add = new Add();
+				add.setValue(a, b);
 				System.out.println(">> "+add.calculate());
 				break;
 			case "-":
 				Sub sub = new Sub();
+				sub.setValue(a, b);
 				System.out.println(">> "+sub.calculate());
 				break;
 			case "*":
 				Mul mul = new Mul();
+				mul.setValue(a, b);
 				System.out.println(">> "+mul.calculate());
 				break;
 			case "/":
 				Div div = new Div();
+				div.setValue(a, b);
 				System.out.println(">> "+div.calculate());
 				break;
 			case "/q":
